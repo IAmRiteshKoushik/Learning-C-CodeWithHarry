183601b21144abeea6739c3100b30afbdf86cd93
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2023-03-12 16:26:51 +0530

    Modified 05

--- chapter-07-arrays/05_pointer_arithmetic.c
+++ chapter-07-arrays/05_pointer_arithmetic.c
@@ -10,12 +10,27 @@ int main(){
 
     // Figure out the basics of hexadecimals and use
     // %p for printing out the hex values.
-
+// ----------------------------------------------------------
+    // Integer Pointer - Memory Size (4)
     int i =34;
     int *ptr = &i;
     printf("The value of ptr is %p\n", ptr);
     ptr++;
     printf("The value of ptr is %p\n", ptr);
+
+    // Character Pointer - Memory Size (1)
+    char c = 34;
+    char *cptr = &c;
+    printf("The value of cptr is %p\n", cptr);
+    cptr++;
+    printf("The value of cptr is %p\n", cptr);
+
+    // Float Pointer - Memory Size (4)
+    float f =34.2;
+    float *fptr = &f;
+    printf("The value of fptr is %p\n", fptr);
+    fptr++;
+    printf("The value of fptr is %p\n", fptr);
     return(0);
 
 }

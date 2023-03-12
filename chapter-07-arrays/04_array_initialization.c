715f6c1df84d71a3a62a3e9ff40f7edd2cc700fd
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2023-03-12 12:16:06 +0530

    Add 04_array_initialization.c

---
+++ chapter-07-arrays/04_array_initialization.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    
+    // Filling out an array during declaration
+    int arr[] = {34, 232, 23};
+    printf("The value of a[0] is %d\n", arr[0]);
+    printf("The value of a[0] is %d\n", arr[1]);
+    printf("The value of a[0] is %d\n", arr[2]);
+    
+    float arr2[] = {34, 232, 23};
+    printf("The value of a[0] is %f\n", arr2[0]);
+    printf("The value of a[0] is %f\n", arr2[1]);
+    printf("The value of a[0] is %f\n", arr2[2]);
+
+    return(0);
+}

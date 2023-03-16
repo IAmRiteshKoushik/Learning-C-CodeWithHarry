d1456c7f7d05ee50e491d49f51ec3e36e7781db7
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2023-03-16 11:17:33 +0530

    Intro to pointers

--- chapter-06-pointers/01_pointers.c
+++ chapter-06-pointers/01_pointers.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 
 int main(){
-    
+
+    // Declare a pointer
+    int *j;
+    // Other pointers  --
+    // (Don't get confused with the naming scheme)
+    // int *ch_ptr;
+    // char *ch_ptr;
+    // float *ch_ptr;
+
     return(0);
 }

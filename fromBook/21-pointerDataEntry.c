54db202f3c08b3008b4e5e23720e470651aa689f
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2023-06-15 11:10:13 +0530

    Usage of fgets() function

--- fromBook/21-pointerDataEntry.c
+++ fromBook/21-pointerDataEntry.c
@@ -1,9 +1,16 @@
 #include<stdio.h>
 
 int main(){
-    char food[5];
+    /* char food[5];
     printf("Enter your favourite food: ");
     scanf("%s", food);
-    printf("Favourite food: %s\n", food);
+    printf("Favourite food: %s\n", food); */
+
+    /* Using fgets() as an alternative to scanf() */
+    char food[5];
+    printf("Enter favourite food: ");
+    fgets(food, sizeof(food), stdin);
+    /* This accepts only 3 charactes and the 4th character is \0 */
+    printf("%s", food);
     return(0);
 }
